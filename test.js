610b22381287f288b95e564e3d3a123e77ef79ed
unknown <yonitejami11@gmail.com>
2024-02-04 20:58:33 +0200

    Version 1

---
+++ test.js
@@ -0,0 +1,9 @@
+let nbrClasse = 100;
+let nbreEleve = 20;
+
+let inscription = prompt("combien d eleve sont inscrit ? ")
+let classePleine= inscription/nbrClasse;//5
+let eleveClasse = nbrClasse/nbreEleve;
+console.log(eleveClasse);
+
+console.log(classePleine);
